17418256f6e4fa52bd4c2072d639815255f36ebf
Dmitry Zinushin <zinushin@gmail.com>
2020-03-16 11:24:51 +0300

    add in init.sh some staff

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 # install brew deps
 brew install git mc bat httpie p7zip python
@@ -13,3 +14,7 @@ brew cask install \
 brew install \
     cloudfoundry/tap/bosh-cli \
     cloudfoundry/tap/credhub-cli
+
+# custom scripts
+mkdir -p ~/bin
+cp ./bin/* ~/bin/
