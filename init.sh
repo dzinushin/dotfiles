777f2b8cbefadb4efb88ae7722433f5ddbf48f55
Dmitry Zinushin <zinushin@gmail.com>
2020-03-16 13:20:16 +0300

    more stuff

--- init.sh
+++ init.sh
@@ -16,5 +16,17 @@ brew install \
     cloudfoundry/tap/credhub-cli
 
 # custom scripts
-mkdir -p ~/bin
-cp ./bin/* ~/bin/
+mkdir -p $HOME/bin
+cp ./bin/* $HOME/bin/
+
+# install fonts
+cd $HOME/Library/fonts && \
+curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Bold.ttf && \
+curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Light.ttf && \
+curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Medium.ttf && \
+curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Regular.ttf && \
+curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf
+cd -
+
+# VS Code config file
+cp ./vscode/settings.json "$HOME/Library/Application Support/Code/User/"
