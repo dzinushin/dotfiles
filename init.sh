fbc55571884e172683dfaf43de027ca421e5df8a
Dmitry Zinushin <zinushin@gmail.com>
2020-03-16 11:20:47 +0300

    add init.sh script for initial setup

---
+++ init.sh
@@ -0,0 +1,15 @@
+
+# install brew deps
+brew install git mc bat httpie p7zip python
+
+brew cask install \
+    jetbrains-toolbox \
+    visual-studio-code \
+    keepassxc \
+    slack \
+    sublime-text \
+    robo-3t
+
+brew install \
+    cloudfoundry/tap/bosh-cli \
+    cloudfoundry/tap/credhub-cli
