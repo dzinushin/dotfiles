cc0db2842bf97fa1b9b4f0c400b88940ba9f78e1
Dmitry Zinushin <zinushin@gmail.com>
2020-03-22 14:47:33 +0300

    small improvements

--- init.sh
+++ init.sh
@@ -1,32 +1,46 @@
 #!/bin/sh
 
 # install brew deps
-brew install git mc bat httpie p7zip python jq fzf nvm
+brew install git \
+    python \
+    mc \
+    bat \
+    httpie \
+    p7zip \
+    jq \
+    fzf \
+    nvm
 
+# optionally also can be installed: use-engine zoomus
 brew cask install \
     jetbrains-toolbox \
     visual-studio-code \
     keepassxc \
     slack \
     sublime-text \
-    robo-3t
+    robo-3t \
+    tunnelbear
 
 brew install \
     cloudfoundry/tap/bosh-cli \
-    cloudfoundry/tap/credhub-cli
+    cloudfoundry/tap/credhub-cli \
+    mongodb/brew/mongodb-community-shell # https://github.com/mongodb/homebrew-brew
+
+# install fonts
+cd $HOME/Library/fonts && 
+curl --silent -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Bold.ttf &&  \
+curl --silent -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Light.ttf && \
+curl --silent -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Medium.ttf && \
+curl --silent -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Regular.ttf && \
+curl --silent -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf
+cd -
 
 # custom scripts
 mkdir -p $HOME/bin
 cp ./bin/* $HOME/bin/
 
-# install fonts
-cd $HOME/Library/fonts && \
-curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Bold.ttf && \
-curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Light.ttf && \
-curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Medium.ttf && \
-curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Regular.ttf && \
-curl -O https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf
-cd -
-
 # VS Code config file
 cp ./vscode/settings.json "$HOME/Library/Application Support/Code/User/"
+
+# mongo cli
+cp .mongorc.js $HOME/
