c65c1b7d29af5066271e7c83657ff2228c9b7f75
Dmitry Zinushin <zinushin@gmail.com>
2025-02-13 01:57:38 +0300

    add staff

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -12,6 +12,9 @@ vim.opt.relativenumber = true
 vim.opt.clipboard = 'unnamedplus'
 
 vim.keymap.set("n", "<leader>pv",vim.cmd.Ex)
+vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<leader>x", ":.lua<CR>")
+vim.keymap.set("v", "<leader>x", ":lua<CR>")
 
 require('config.lazy')
 
