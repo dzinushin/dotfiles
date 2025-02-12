c58b5c7494c31e131a9dd89e7b77f94a3f5c3044
Dmitry Zinushin <zinushin@gmail.com>
2025-02-13 01:09:17 +0300

    add staff

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -6,6 +6,8 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.clipboard = 'unnamedplus'
 
