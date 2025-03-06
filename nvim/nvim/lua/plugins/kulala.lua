b87d7aec99f9b4eee8e5377494a26325a1bf815b
Dmitry Zinushin <zinushin@gmail.com>
2025-03-06 21:12:01 +0300

    add staff

--- nvim/nvim/lua/plugins/kulala.lua
+++ nvim/nvim/lua/plugins/kulala.lua
@@ -1,4 +1,11 @@
 return {
-  'mistweaverco/kulala.nvim',
-  opts = {}
+  {
+    'mistweaverco/kulala.nvim',
+    keys = {"<leader>Rs", "<leader>Ra", "<leader>Ro"},
+    ft = {"http", "rest"},
+    opts = {
+      -- your configuration comes here
+      global_keymaps = false,
+    },
+  },
 }
