f7dffeb4ea7a9d4ac0a5017428eb5e20b47aa0c9
Dmitry Zinushin <zinushin@gmail.com>
2025-03-24 11:14:37 +0300

    add staff

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { "c", "lua", "vim", "http", "vimdoc", "markdown", "markdown_inline", "query", "rust", "python", "javascript", "html", "kotlin", "json", "yaml" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "http", "vimdoc", "markdown", "markdown_inline", "query", "rust", "python", "javascript", "html", "kotlin", "json", "yaml" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end
+  }
 }
