157c968f0206e2333aed511e415d6752f6c15261
Dmitry Zinushin <zinushin@gmail.com>
2026-09-11 21:59:39 +0300

    nvim: маппинги telescope объявлены один раз, в keys
    Было два списка: goлые строки-триггеры в keys и сами маппинги в config.
    Они уже разъехались по стилю (<leader>gf строкой-командой, остальные
    функциями), и which-key до загрузки плагина не видел ни одного desc.
    
    Теперь клавиши объявлены прямо в keys с описаниями — lazy сам делает их
    триггерами загрузки. Из config ушли 12 вызовов map(), из file-browser —
    весь config. Плагины остаются ленивыми.

--- dot_config/nvim/lua/plugins/telescope-file-browser.lua
+++ dot_config/nvim/lua/plugins/telescope-file-browser.lua
@@ -1,18 +1,16 @@
 return {
   "nvim-telescope/telescope-file-browser.nvim",
   dependencies = {
-    "nvim-telescope/telescope.nvim", 
-    "nvim-lua/plenary.nvim" 
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim"
   },
-  keys = { '<leader>bf' },
-  config = function()
-    local map = vim.keymap.set
-
-    map('n','<leader>bf', 
+  keys = {
+    {
+      '<leader>bf',
       function()
-	     require("telescope").extensions.file_browser.file_browser()
+        require("telescope").extensions.file_browser.file_browser()
       end,
-      { desc = 'Telescope file browser' }
-    )
-  end,
+      desc = 'Telescope file browser',
+    },
+  },
 }
--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,12 @@
+-- маппинги объявлены прямо в keys: lazy сам делает их триггерами загрузки,
+-- и which-key видит desc ещё до того, как плагин поднялся. отдельного списка
+-- клавиш в config больше нет — расходиться нечему
+local function builtin(name, opts)
+  return function()
+    require("telescope.builtin")[name](opts)
+  end
+end
+
 return {
   'nvim-telescope/telescope.nvim',
   -- без version: последний релизный тег (v0.2.2) — сентябрь 2024, то есть старше
@@ -6,17 +15,24 @@ return {
     'nvim-lua/plenary.nvim'
   },
   cmd = 'Telescope',
-  -- маппинги ставит config, здесь только триггеры загрузки
   keys = {
-    '<leader>ff', '<leader>fr', '<leader>fg', '<leader>fb', '<leader>fh',
-    '<leader>ed', '<leader>en',
-    '<leader>gb', '<leader>gc', '<leader>gs', '<leader>gt', '<leader>gf',
+    { '<leader>ff', builtin('find_files'), desc = 'Telescope find files' },
+    { '<leader>fr', builtin('oldfiles'),   desc = 'Telescope recent files' },
+    { '<leader>fg', builtin('live_grep'),  desc = 'Telescope live grep' },
+    { '<leader>fb', builtin('buffers'),    desc = 'Telescope buffers' },
+    { '<leader>fh', builtin('help_tags'),  desc = 'Telescope help tags' },
+
+    { '<leader>ed', builtin('find_files', { cwd = '~/dotfiles', hidden = true }), desc = 'Telescope browse dotfiles' },
+    { '<leader>en', builtin('find_files', { cwd = vim.fn.stdpath('config') }),    desc = 'Telescope browse nvim config files' },
+
+    { '<leader>gb', builtin('git_branches'), desc = 'Git branches' },
+    { '<leader>gc', builtin('git_commits'),  desc = 'Git commits' },
+    { '<leader>gs', builtin('git_status'),   desc = 'Git status' },
+    { '<leader>gt', builtin('git_stash'),    desc = 'Git stash' },
+    { '<leader>gf', builtin('git_files'),    desc = 'Git files' },
   },
   config = function()
-    -- Telescope config
-    local builtin = require('telescope.builtin')
     local actions = require('telescope.actions')
-    local map = vim.keymap.set
 
     require('telescope').setup{
       defaults = {
@@ -30,11 +46,6 @@ return {
           },
         },
         mappings = {
---          i = {
---            ['<C-k>'] = actions.move_selection_previous, -- move to prev result
---            ['<C-j>'] = actions.move_selection_next, -- move to next result
---            ['<C-l>'] = actions.select_default, -- open file
---          },
           n = {
             ['q'] = actions.close,
           },
@@ -64,37 +75,5 @@ return {
         },
       },
     }
-
-    map('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    map('n', '<leader>fr', builtin.oldfiles, { desc = 'Telescope recent files' })
-    map('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    map('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    map('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-    map('n', '<leader>ed',
-      function()
-        local opts = {
-          cwd = "~/dotfiles",
-          hidden = true
-        }
-        builtin.find_files(opts)
-      end,
-      { desc = 'Telescope browse dofiles' }
-    )
-    map('n', '<leader>en',
-      function()
-        local opts = {
-          cwd = vim.fn.stdpath("config")
-        }
-        builtin.find_files(opts)
-      end,
-      { desc = 'Telescope browse nvim config files' }
-    )
-
-    map('n', '<leader>gb', builtin.git_branches, { desc = 'Git branches' })
-    map('n', '<leader>gc', builtin.git_commits, { desc = 'Git commits' })
-    map('n', '<leader>gs', builtin.git_status, { desc = 'Git status' })
-    map('n', '<leader>gt', builtin.git_stash, { desc = 'Git stash' })
-    map("n", "<leader>gf", ":Telescope git_files<CR>", { desc = "Git: Files" })
   end,
 }
